930ed8ff2cf9b2d4102138b0d5226d3be27e1ea2
Jakub Pekař <jakub.pekar@id-sign.com>
2020-03-12 12:16:44 +0100

    Rebuild

--- dist/jspdf.node.debug.js
+++ dist/jspdf.node.debug.js
@@ -3,8 +3,8 @@
 /** @license
  *
  * jsPDF - PDF Document creation from JavaScript
- * Version 1.5.3 Built on 2020-03-12T00:30:01.108Z
- *                      CommitID 364c472689
+ * Version 1.5.3 Built on 2020-03-12T11:14:52.510Z
+ *                      CommitID eb07b35da9
  *
  * Copyright (c) 2010-2018 James Hall <james@parall.ax>, https://github.com/MrRio/jsPDF
  *               2015-2018 yWorks GmbH, http://www.yworks.com
@@ -47,6 +47,48 @@ function _typeof(obj) {
   }
 
   return _typeof(obj);
+}
+
+function _slicedToArray(arr, i) {
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest();
+}
+
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+
+function _iterableToArrayLimit(arr, i) {
+  if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) {
+    return;
+  }
+
+  var _arr = [];
+  var _n = true;
+  var _d = false;
+  var _e = undefined;
+
+  try {
+    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+      _arr.push(_s.value);
+
+      if (i && _arr.length === i) break;
+    }
+  } catch (err) {
+    _d = true;
+    _e = err;
+  } finally {
+    try {
+      if (!_n && _i["return"] != null) _i["return"]();
+    } finally {
+      if (_d) throw _e;
+    }
+  }
+
+  return _arr;
+}
+
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance");
 }
 
 /* eslint-disable no-console */
@@ -10831,10 +10873,18 @@ global.tmp = jsPDF;
     var padding = this.internal.__cell__.padding;
     var fontSize = this.internal.__cell__.table_font_size;
     var scaleFactor = this.internal.scaleFactor;
-    return Object.values(model).map(function (value) {
-      return _typeof(value) === "object" ? value.text : value;
-    }).map(function (value) {
-      return this.splitTextToSize(value, columnWidths[value] - padding - padding);
+    return Object.entries(model).map(function (_ref) {
+      var _ref2 = _slicedToArray(_ref, 2),
+          key = _ref2[0],
+          value = _ref2[1];
+
+      return _typeof(value) === "object" ? [key, value.text] : [key, value];
+    }).map(function (_ref3) {
+      var _ref4 = _slicedToArray(_ref3, 2),
+          key = _ref4[0],
+          value = _ref4[1];
+
+      return this.splitTextToSize(value, columnWidths[key] - padding - padding);
     }, this).map(function (value) {
       return this.getLineHeightFactor() * value.length * fontSize / scaleFactor + padding + padding;
     }, this).reduce(function (pv, cv) {
